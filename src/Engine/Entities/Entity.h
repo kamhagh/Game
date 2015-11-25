1edb2c245849bf02a8a6048887694bed361a48f1
Arman Imani <armanimani87@gmail.com>
2015-11-25 15:36:06 +0330

    added entity

--- src/Engine/Entities/Entity.h
+++ src/Engine/Entities/Entity.h
@@ -1,5 +1,6 @@
 #pragma once;
 #include "src\Engine\Model\ShapeData.h"
+#include <glm\glm.hpp>
 
 class Entity{
 
@@ -7,7 +8,12 @@ public:
 
 	Entity(ShapeData &shape) : shape(&shape){}
 	inline ShapeData &getShape(){ return *shape; }
+	
 	ShapeData *shape;
+	glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 scale = glm::vec3(0.0f, 0.0f, 0.0f);
+
 private:
 	
 };
